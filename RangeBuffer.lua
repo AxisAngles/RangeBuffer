7dabefbaaf3e4869f06d8b30393ec731a570cf5e
AxisAngles <AxisAngles@Gmail.com>
2024-10-10 23:14:18 -0700

    Update RangeBuffer.lua
    No longer returns a multiple of 6 bytes

--- RangeBuffer.lua
+++ RangeBuffer.lua
@@ -23,8 +23,7 @@ function RangeBuffer.newWriter(): RangeWriterObj
 	self._c = 1
 	self._v = 0
 
-	--self.realLength = 0
-	--self.idealLength = 0
+	--self._bitLoss = 0
 
 	return self
 end
@@ -32,6 +31,7 @@ end
 function RangeBuffer.newReader(buff): RangeReaderObj
 	local self = setmetatable({}, RangeReader)
 
+	self._size = buffer.len(buff)
 	self._head = 0
 	self._buffer = buff
 
@@ -56,12 +56,11 @@ end
 
 -- n is the number of options, v is the value, v < n
 function RangeWriter:write(n, v)
-	--self.idealLength += math.log(n, 2)
 	while n*self._c > max do
 		-- factor0 * factor1 >= n
 		local f0 = max//self._c
 		local f1 = -(-n//f0)
-		--self.realLength += 48
+		--self._bitLoss += math.log(max*f1/(self._c*n), 2)
 		self:_writeBuffer48(self._v + self._c*(v%f0))
 		self._v = 0
 		self._c = 1
@@ -74,10 +73,13 @@ function RangeWriter:write(n, v)
 end
 
 function RangeWriter:dump()
-	--self.realLength += math.ceil(math.log(self._c, 2))
-	self:_writeBuffer48(self._v)
-
 	local length = self._head
+	local c = 1
+	while c < self._c do
+		length += 1
+		c *= 256
+	end
+
 	for _, buff in next, self._buffers do
 		length += buffer.len(buff)
 	end
@@ -90,22 +92,42 @@ function RangeWriter:dump()
 	end
 
 	buffer.copy(finalBuff, offset, self._buffer, 0, self._head)
+	offset += self._head
+	
+	local v = self._v
+	for i = offset, length - 1 do
+		buffer.writeu8(finalBuff, i, v)
+		v //= 256
+	end
+	
 	return finalBuff
 end
 
 function RangeReader:_readBuffer48()
-	local v0  = buffer.readu32(self._buffer, self._head    )
-	local v32 = buffer.readu16(self._buffer, self._head + 4)
-	local v = v0 + 2^32*v32
 	self._head += 6
+	
+	if self._head > self._size then
+		local v = 0
+		local c = 1
+		for i = self._head - 6, self._size - 1 do
+			local vi = buffer.readu8(self._buffer, i)
+			v += c*vi
+			c *= 256
+		end
+		return v
+	end
+	
+	local v0  = buffer.readu32(self._buffer, self._head - 6)
+	local v32 = buffer.readu16(self._buffer, self._head - 2)
+	local v = v0 + 2^32*v32
 
 	return v
 end
 
 -- n is the number of options
 function RangeReader:read(n)
-	local c = 1
 	local v = 0
+	local c = 1
 	-- c goes up as we read
 	while n*self._c > max do
 		-- factor0 * factor1 >= n
@@ -150,8 +172,11 @@ type RangeReaderMeta = {
 }
 
 type RangeReaderObj = typeof(setmetatable({} :: {
+	_size: number;
 	_head: number;
 	_buffer: buffer;
 	_c: number;
 	_v: number;
 }, {} :: RangeReaderMeta))
+
+return RangeBuffer
